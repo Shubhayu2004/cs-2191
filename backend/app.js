dc028423716a5a909479da2b5207271c588e1bd5
Shubhayu Sarkar <shubhayu.sarkar02@gmail.com>
2024-12-05 09:25:06 +0530

    The Backend Login has been done

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectTodb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 connectTodb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.get('/' , (req,res) => {
     res.send('Hello world');
 });
 
+app.use('/users', userRoutes);
 module.exports = app;
